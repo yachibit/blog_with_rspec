c1b2ef5e10aab8ecf2a8f86d103fc03ba33d66c4
yachibit <y.yachix@gmail.com>
2013-06-09 23:23:23 +0900

    Add index action at ArticlesController

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,25 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe ArticlesController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    before do
+      @article1 = Article.create(
+        title: "Article 1",
+        body: "Hello"
+      )
+      @article2 = Article.create(
+        title: "Article 2",
+        body: "World"
+      )
       get 'index'
+    end
+    it "returns http success" do
       response.should be_success
     end
+    it "登録されている記事をすべて返すこと" do
+      assigns[:articles].should =~ [@article1, @article2]
+    end
   end
-
 end
