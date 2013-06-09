82b9d7e120b1caca012732e474f86c6ea1b75631
yachibit <y.yachix@gmail.com>
2013-06-09 23:43:43 +0900

    Fix view at articles/index

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,5 +1,22 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe "articles/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    assign(
+      :articles, [
+        Article.create(
+          title: "初めてのブログ",
+          body: "こんにちは"
+        )
+      ]
+    )
+    render
+    it "記事のタイトルが表示されること" do
+      rendered.should =~ /初めてのブログ/
+    end
+    it "記事の本文が表示されること" do
+      rendered.should =~ /こんにちは/
+    end
+  end
 end
